824d3aa77a7bfaa6d64f6bb8c84006cddec64a12
znarf1587 <fferrufino.rq@gmail.com>
2018-02-28 21:12:25 -0400

    add Models/Conexion.php, archivo que contiene la comunicación con la base de datos

---
+++ Models/Conexion.php
@@ -0,0 +1,28 @@
+<?php
+  namespace Models;
+
+  class Conexion{
+
+    private $datos = array(
+      "host" => "localhost",
+      "user" => "root",
+      "pass" => "",
+      "db" => "store"
+    );
+
+    private $con;
+
+    public function __construct(){
+      $this->con = new \mysqli($this->datos['host'], $this->datos['user'], $this->datos['pass'], $this->datos['db']);
+    }
+
+    public function consultaSimple($sql){
+      $this->con->query($sql);
+    }
+
+    public function consultaRetorno($sql){
+      $c = $this->con->query($sql);
+      return $c;
+    }
+  }
+?>
