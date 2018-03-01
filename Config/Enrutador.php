b6a77d772afbb6608d846cd901b8e108c7c0a7d6
znarf1587 <fferrufino.rq@gmail.com>
2018-02-28 21:15:42 -0400

    Add Config/ archivos de configuracion de rutas y carga automatica de controladores

---
+++ Config/Enrutador.php
@@ -0,0 +1,29 @@
+<?php
+  namespace Config;
+
+  class Enrutador{
+
+    public static function run(Request $request){
+      $controlador = $request->getControlador() . "Controller";
+      $ruta = ROOT . "Controllers" . DS . $controlador . ".php";
+      $metodo = $request->getMetodo();
+
+      if($metodo == "index.php"){
+        $metodo = "index";
+      }
+
+      $argumento = $request->getArgumento();
+      if(is_readable($ruta)){
+        require_once $ruta;
+        $mostrar = "Controllers\\" . $controlador;
+        $controlador = new $mostrar;
+        if(!isset($argumento)){
+          call_user_func(array($controlador, $metodo));
+        }else{
+          call_user_func_array(array($controlador, $metodo), $agumento);
+        }
+      }
+    }
+  }
+
+?>
